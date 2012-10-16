d126e3cb5543f872bd952531c6d1b506369d9af0
CSquared <tech@ucdavis.edu>
2012-10-15 21:05:05 -0700

    Basic readsa()

--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 
 using namespace std;
 
-/*
- * 
- */
+int readsa(int length, int i)
+{
+  for (int j = 0; j < length_CA / 2; j++) // length_CA = CA - 1 int
+  {
+    if ((i >= CA[j * 2]) && (i < CA[j * 2 + 1] + CA[j * 2]))
+      Put 1 in EBP;
+  }
+  
+  Put 0 in EBP;
+}
+
 int main(int argc, char** argv) {
 
   return 0;
