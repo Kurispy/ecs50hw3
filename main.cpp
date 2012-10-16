6394e3b99c5fca93bdb4d23a99c5ae8f990c1a0f
CSquared <tech@ucdavis.edu>
2012-10-15 19:50:11 -0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: csquared
+ *
+ * Created on October 15, 2012, 7:32 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+  return 0;
+}
+
