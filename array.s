b659a1c6b275386255c7925d8dcd11ecad47420d
CSquared <tech@ucdavis.edu>
2012-10-21 18:05:41 -0700

    Properly sets eax

--- array.s
+++ array.s
@@ -7,44 +7,60 @@ LUA:
   .long 2
 
 p:
-  .long $LUA #pLUA
+  .long 0 #pLUA
   .long 2 #request index
+# EAX Set
 
-movl $p, %eax
-#
 l_UA:
-  .long (%eax)
+  .long 0 #(%eax)
 
 l_CA:
-  .long (%eax) + 4 
+  .long 0 #(%eax) + 4
 
 CA:
-  .long (%eax) + 8
+  .long 0 #(%eax) + 8
 
 index:
-  .long %eax + 4
-
-movl (l_CA), %eax
-idivl $2 # %eax is now l_CA / 2
-
-movl $0, %ebx # EBX will serve as the current index for CA
-  
+  .long 0 #%eax + 4
 
 .text # code section
 
 .globl _start
 _start:
+
+init:
+  movl $LUA, %ebx
+  movl %ebx, p
+  movl $p, %eax
+
+  movl (%eax), %ebx
+  movl (%ebx), %ebx
+  movl %ebx, l_UA
   
+  movl (%eax), %ebx
+  movl 4(%ebx), %ebx 
+  movl %ebx, l_CA
 
-# (%eax) = address of length of UA; (%eax) + 4 = address of length of CA; (%eax) + 8 = address of CA; %eax + 4 = index
+  movl 8(%eax), %ebx
+  movl %ebx, CA
 
-  movl CA, %ecx #temp
-	incl (%ecx, %ebx, 16) #go to CA[2*j]
+  movl 4(%eax), %ebx
+  movl %ebx, index
+
+  movl l_CA, %eax
+  movl $2, %ecx
+  idivl %ecx # %eax is now l_CA / 2
+
+  movl $0, %ebx # EBX will serve as the current index for CA
+
+readsa:  
+  	movl CA, %ecx #temp
+	incl (%ecx, %ebx, 8) #go to CA[2*j]
 	cmpl %ecx, index #first part of if i>=ca[2*j]
 	jl jump_for #jump if CA < index
 	
 	movl CA, %ecx #temp
-	addl $8, %ecx #ca[2*j+1]
+	addl $4, %ecx #ca[2*j+1]
 	cmpl %ecx, index #second part of if
 	jge jump_for #jump to iteration if >=0
 	movl $1, %ebp #store 1 in %ebp if correct
@@ -56,12 +72,5 @@ jump_for:
 	jnz readsa #exit loop when j=l_CA
 	movl $0, %ebp #else store 0 in %ebp
 	ret
-
-  
-  
-
-writea:
   
 done:
-
-
